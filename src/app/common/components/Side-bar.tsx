2ac43f13d133792641c636c9ed9525f6af39063b
RamSterB <113079935+RamSterB@users.noreply.github.com>
2025-05-06 21:51:44 -0500

    feat: Update Navbar and SideBar components to display user information and add icon; modify package.json and layout metadata

--- src/app/common/components/Side-bar.tsx
+++ src/app/common/components/Side-bar.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   Card,
   CardHeader,
@@ -10,12 +11,15 @@ import {
 import SideBarCards from "./SidebarCards";
 
 import { mockDataGroup, mockDataMain } from "../utils/sideBarContent";
+import { useAuth } from "@/app/contexts/AuthProvider";
 
 export default function SideBar() {
+  const { user , loading } = useAuth()
   return (
     <div className="fixed left-0 top-[75px] ml-4 space-y-5 hidden lg:block">
       <SideBarCards items={mockDataMain} />
-      <SideBarCards title="Pinned Groups" items={mockDataGroup} />
+      {user ? (<SideBarCards title="Pinned Groups" items={mockDataGroup} />) : null}
+      
     </div>
   );
 }
